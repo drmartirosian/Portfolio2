fb8f4ffd8ec92b11fcbc270d755ec77ecc8492f9
drmartirosian <55171534+drmartirosian@users.noreply.github.com>
2020-03-05 21:05:24 -0600

    Links up

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ import junky from './img/junky.png'
 import dom from './img/dom.png'
 import coverletter from './img/coverletter.png'
 import resume from './img/resume.png'
+import propic from './img/propic.jpeg'
 import Card from './components/Card'
-
+import { FaLinkedin, FaGithubAlt,FaTwitterSquare, FaGoogle,FaPhone } from 'react-icons/fa';
 
 
 class App extends Component {
@@ -29,8 +30,8 @@ class App extends Component {
         <div className="bg-image"></div>
           <div className="content-wrap">
           <h1>Daniel <strong>Martirosian</strong></h1>
-          <p>Description here...</p>
-          <h3>Let's<br></br> <a href="#section-b" className="btn">START</a></h3>
+          <p>Software Engineer</p>
+          <h3><br></br> <a href="#main-footer" className="btn">Contact Info</a></h3>
         </div>
       </header>
 
@@ -41,11 +42,9 @@ class App extends Component {
 
         <section id="section-a" className="grid">
           <div className="content-wrap">
-            <h2 className="content-title">Software Engineer</h2>
-            <div className="content-text">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe sint eligendi possimus? Unde officiis magnam laborum ipsa distinctio odio, vero dolores dicta aliquam aperiam repellendus. Perferendis officiis deserunt velit voluptas nobis sequi
-            animi totam, accusantium, ex eius quia, natus quo?</p>
-            </div>
+            <img style={{width:'50%', height:'50%'}} src={propic} />
+            <h2 className="content-title">Mission Statement:</h2>
+            <p>Software developer geared towards getting the product to market. I see shortcomings or weaknesses as opportunities for growth. I am at peak performance when collaborating with and learning from a team of talented developers. Building code to thrive in tomorrow's market requires building teams that can do the same. 620+ hours of full-stack software program engineering. Knowledge spans Javascript, HTML, React, Python, JavaScript, Django, Express, MongoDB, PostgreSQL server, and more. Let's get coding! </p>
           </div>
         </section>
 
@@ -57,13 +56,13 @@ class App extends Component {
         <div id="section-b" className="grid">
           <ul>
             <li>
-              <Card img={dom} title='DomDebugger'  description='A' btnLink='http://Linkedin.com/in/drmartirosian' />
+              <Card img={dom} title='DomDebugger'  description='Simple educational DOM-manipulation game. Enter code in the side panel to fix the "website" and progress the levels.' btnLink='https://codepen.io/drmartirosian/pen/bGGMmbG?editors=1010' />
             </li>
             <li>
-              <Card img={junky} title='JunKart'  description='B' btnLink='http://Linkedin.com/in/drmartirosian' />
+              <Card img={junky} title='JunKart'  description='Simple market e-commerce app. Uses React, JavaScript, Paypal API. Deployed with Netlify.com. Visit Github for code:' btnLink='http://junkycart.netlify.com/' />
             </li>
             <li>
-              <Card img={panda} title='RedPanda'  description='C' btnLink='http://Linkedin.com/in/drmartirosian' />
+              <Card img={panda} title='RedPanda'  description='A coder / programmer paradise! Blog, chat, and post about the latest/greatest projects you have in the pipeline.' btnLink='https://redpanda2.herokuapp.com/' />
             </li>
           </ul>
         </div>
@@ -74,9 +73,14 @@ class App extends Component {
 
         <section id="section-c" className="grid">
           <div className="content-wrap">
-            <h1 className="content-title">SECTION:</h1>
-            <h3>adgdfasfsd sfsdf s sd fsd sd fs</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe sint eligendi possimus? Unde officiis magnam laborum ipsa distinctio odio, vero dolores dicta aliquam aperiam repellendus. Perferendis officiis deserunt velit voluptas nobis sequ</p>
+            <h1 className="content-title">LANGUAGES:</h1>
+            <p>JavaScript, Python</p>
+            <h1 className="content-title">FRAMEWORKS:</h1>
+            <p>React, Express, Django</p>
+            <h1 className="content-title">DATABASES:</h1>
+            <p>MongoDB, PostgreSQL</p>
+            <h1 className="content-title">AND MORE!</h1>
+            <p>AWS/Cloudinary, Hooks/classes, Github/Git, and more!</p>
           </div>
         </section>
 
@@ -108,8 +112,12 @@ class App extends Component {
 
 
       <footer id="main-footer" className="grid">
-        <div>Acme Web Solutions</div>
-        <div>Project By <a href="http://Linkedin.com/in/drmartirosian" target="_blank" rel="noopener noreferrer">LinkedIn</a></div>
+        <a href="https://twitter.com/drmartirosian" target="_blank" rel="noopener noreferrer"><FaTwitterSquare />Twitter</a>
+        <a href="http://github.com/drmartirosian" target="_blank" rel="noopener noreferrer"><FaGithubAlt />Github</a>
+        <a href="http://Linkedin.com/in/drmartirosian" target="_blank" rel="noopener noreferrer"><FaLinkedin />LinkedIn</a>
+        <br></br>
+        <h4> <FaGoogle /> martirosianmd@gmail.com</h4>
+        <h4> <FaPhone /> 619.905.1075</h4>
       </footer>
     </div>
     );
